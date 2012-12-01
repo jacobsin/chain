1615eb5c1cdd2bb0c9b0c68f7993baadf800982d
Jacob Sin <jacob.sin@gmail.com>
2012-12-02 01:53:47 +0800

    split comparison class out

--- src/main/java/chain/WordChain.groovy
+++ src/main/java/chain/WordChain.groovy
@@ -47,27 +47,3 @@ class WordChain {
 
 }
 
-class WordComparison {
-  def differenceCount = 0
-  def nextStepPatterns = []
-  String word1
-  String word2
-
-  WordComparison(word1, word2) {
-    this.word1 = word1
-    this.word2 = word2
-    compare()
-  }
-
-  def compare() {
-    [word1.toCharArray(), word2.toCharArray()].transpose().eachWithIndex{it, i->
-      if (it[0] != it[1]) {
-        differenceCount++
-
-        def nextStepPattern = new StringBuilder(word1)
-        nextStepPattern.setCharAt(i, '.' as char)
-        nextStepPatterns << nextStepPattern.toString()
-      }
-    }
-  }
-}
