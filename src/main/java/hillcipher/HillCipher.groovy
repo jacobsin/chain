62c5ef6c017d9dd7ef8fa26801f4e4d2e85f888f
Jacob Sin <jacob.sin@gmail.com>
2012-12-17 00:53:05 +0800

    hill cipher wip - decrypt with original encryption key

--- src/main/java/hillcipher/HillCipher.groovy
+++ src/main/java/hillcipher/HillCipher.groovy
@@ -4,25 +4,66 @@ import org.math.array.Matrix
 
 class HillCipher {
 
-  private static final org.math.array.Matrix DEFAULT_KEY = new Matrix([
+  static final Matrix DEFAULT_KEY = new Matrix([
       [6, 24, 1],
       [13, 16, 10],
       [20, 17, 15]
-  ])
-  private static final org.math.array.Matrix INVERSED_KEY = new Matrix([
-      [8, 5, 10],
-      [21, 8, 21],
-      [21, 12, 8]
   ])
 
   private static final char A_CHAR = ('A' as char).charValue()
 
   def decrypt(String encrypted) {
-    fromMod26(INVERSED_KEY * toMod26(encrypted))
+    def inversedKey = inverseMod26(DEFAULT_KEY)
+    fromMod26(inversedKey * toMod26(encrypted))
   }
 
   def encrypt(String decrypted) {
     fromMod26(DEFAULT_KEY * toMod26(decrypted))
+  }
+
+  def inverseMod26(Matrix key) {
+    int det = Matrix.det(key).round()
+    def inversedDet = inverseDetMod26(det)
+    adjucate(key) * inversedDet
+  }
+
+  def adjucate(Matrix matrix) {
+    def a = []
+    def rows = matrix.rowsNumber
+    def columns = matrix.columnsNumber
+    for (int i = 1; i <= rows; i++) {
+      a << []
+      for (int j = 1; j <= columns; j++) {
+        a[-1] << cofactor(matrix,i,j)
+      }
+    }
+    Matrix.transpose(new Matrix(a))
+  }
+
+  double cofactor(Matrix matrix, int i, int j) {
+    Matrix m = subMatrix(matrix, i, j)
+    return (-1)**(i+j) * Matrix.det(m).round()
+  }
+
+  Matrix subMatrix(Matrix matrix, int i, int j) {
+    def a = []
+    def rows = matrix.rowsNumber
+    def columns = matrix.columnsNumber
+    for (int y = 1; y <= rows; y++) {
+      if (y == i) continue;
+      a << []
+      for (int x = 1; x <= columns; x++) {
+        if (x == j) continue;
+        a[-1] << matrix[y][x]
+      }
+    }
+    new Matrix(a)
+  }
+
+  def inverseDetMod26(int det) {
+    (1..25).find {
+      (it * det) % 26 == 1
+    }
   }
 
   def toMod26(String string) {
