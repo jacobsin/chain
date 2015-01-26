206c4f680b711def87cc49b2b9d99014a307e246
Jacob Sin <jacob.sin@gmail.com>
2015-01-27 00:15:45 +0800

    skeleton with gradle

--- src/main/java/chain/NoSolutionFoundException.groovy
+++
@@ -1,8 +0,0 @@
-package chain
-
-
-class NoSolutionFoundException extends RuntimeException {
-  NoSolutionFoundException(String from, String to) {
-    super("no solution found from (${from}) to (${to})")
-  }
-}
